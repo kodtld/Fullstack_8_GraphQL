6adec92ce87e60722c661bc461976a0071317784
kodtld <kodu@live.fi>
2023-03-27 22:29:27 +0300

    8.8- 8.12

---
+++ library-frontend/src/components/Books.js
@@ -0,0 +1,42 @@
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_BOOKS} from '../queries'
+import { useState } from 'react'
+
+const Books = ({show}) => {
+
+  const books = useQuery(ALL_BOOKS)
+
+  if (books.loading) {
+    return <div>loading...</div>
+  }
+
+  if (!show) {
+    return null
+  }
+  
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.data.allBooks.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
